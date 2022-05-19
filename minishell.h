d443b3abb8f4509d1e2cb2beaf49fe25f85669ac
Arshavin Bernita <abernita@pr-f1.kzn.21-school.ru>
2022-05-19 15:26:57 +0300

    1

---
+++ minishell.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abernita <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/19 15:23:28 by abernita          #+#    #+#             */
+/*   Updated: 2022/05/19 15:23:31 by abernita         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stdbool.h>
+
+
+#endif
