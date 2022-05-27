83612b13f2792e77382dbf3e262bc671079e9f45
Arshavin Bernita <abernita@ge-h3.kzn.21-school.ru>
2022-05-27 14:46:22 +0300

    +main ++

--- main.c
+++ main.c
@@ -12,6 +12,18 @@
 
 #include "includes/minishell.h"
 
+int	get_full_path(char **av, char arr[4096])
+{
+	if ((*av)[0] != '.')
+		return (0);
+	if (getcwd(arr, 4096) == 0)
+		return (1);
+	ft_strlcpy(arr + ft_strlen(arr), "/", 2);
+	ft_strlcpy(arr + ft_strlen(arr), av[0], ft_strlen(av[0]) + 1);
+	av[0] = arr;
+	return (0);
+}
+
 int	main(int ac, char **av, char **env)
 {
 	char	*str;
@@ -22,7 +34,7 @@ int	main(int ac, char **av, char **env)
 	get_full_path(av, arr);
 	signal(SIGQUIT, SIG_IGN);
 	if (ac == 2)
-		return (handleav(av));
+		return (handlearg(av));
 	while (1)
 	{
 		signal(SIGINT, SIG_IGN);
