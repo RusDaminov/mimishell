b7b253065e2d01d3423d2a1a1af8acdd2c357d18
Arshavin Bernita <abernita@pr-b2.kzn.21-school.ru>
2022-06-19 22:54:53 +0300

    +exec+

--- execute/execute.c
+++ execute/execute.c
@@ -12,6 +12,46 @@
 
 #include "../includes/execute.h"
 
+static void parent(t_execute *exec, t_cmds *data)
+{
+	if (exec->s_fd != -1)
+		close(exec->sf);
+	exec->s_fd = -1;
+	if (exec->fd[1] != -1)
+		close(exec->fd[1]);
+	exec->fd[1] = -1;
+	exec->s_fd = exec->fd[0];
+	exec->fd[0] = -1;
+	if (data->operators != OPERATORS_NONE)
+	{
+		wait_for_real(exec->lst, exec);
+	}
+}
+
+int	create_childs(t_cmds *data, t_execute *exec)
+{
+	t_pid	*pid;
+	t_list	*tmp;
+
+	signal(SIGINT, SIG_IGN);
+	pid = ft_calloc(sizeof(*pid), 1);
+	if (!pid)
+		return (execute_errors(MALLOC_ERR, exec));
+	tmp = ft_lstnew(pid);
+	if (!tmp)
+		free(pid);
+	if (!tmp)
+		return (execute_errors(MALLOC_ERR, exec));
+	ft_lstadd_back(&(exec->lst), tmp);
+	if (data->write == 1 && pipe(exec->fd) == -1)
+		return (execute_errors(PIPE_ERR, exec));
+	pid->pid = fork();
+	if (pid->pid == 0)
+		exit(child(exec, data));
+	parent(exec, data);
+	return (0);
+}
+
 unsigned char	execute(t_cmds *data)
 {
 	t_execute	exec;
